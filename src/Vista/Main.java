73bfb4cbe693c2878e00fd19efed51b361ce3eec
Cesar-230405 <146404691+Cesar-230405@users.noreply.github.com>
2024-08-03 21:13:09 -0600

    Java

---
+++ src/Vista/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Vista;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculadora_Proyect frm = new Calculadora_Proyect();
+        frm.show();
+    }
+    
+}
